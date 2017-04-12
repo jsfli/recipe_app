bceda56a5394b45976c8788fc0dde7dcd358ad69
Jeffrey Li <jeffrey.li@cantab.net>
2017-04-12 11:55:47 +0800

    added validation functions to all classes

--- lib/instruction.rb
+++ lib/instruction.rb
@@ -1,3 +1,4 @@
 class Instruction < ActiveRecord::Base
   belongs_to(:recipe)
+  validates(:description, :presence => true)
 end
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ActiveRecord::Base
   has_many(:instructions)
   has_many(:ingredients)
   has_and_belongs_to_many(:categories)
+  validates(:name, :presence => true)
+
 end
